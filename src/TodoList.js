c9df256c535e6b867cd949a2061defe080d2f3c7
Dang <ruobin521@gmail.com>
2019-02-18 18:00:38 +0800

    update

---
+++ src/TodoList.js
@@ -0,0 +1,86 @@
+import React, { Component, Fragment } from 'react'
+import './style.css'
+import TodoItem from './todoItem'
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '123',
+      list: [222, 3333]
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleItemDelete = this.handleItemDelete.bind(this)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <label htmlFor="innerInput">请输入内容</label>
+          <input
+            id="innerInput"
+            className="input"
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleBtnClick}>提交</button>
+        </div>
+        <ul>{this.getTodoItem()}</ul>
+      </Fragment>
+    )
+  }
+
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          key={index}
+          index={index}
+          content={item}
+          deleteItem={this.handleItemDelete}
+        />
+      )
+    })
+  }
+
+  handleInputChange(e) {
+    // this.setState({
+    //   inputValue: e.target.value
+    // })
+    // this.setState(() => {
+    //     return {
+    //         inputValue: e.target.value
+    //     }
+    // })
+
+    const value = e.target.value
+    this.setState(() => ({
+      inputValue: value
+    }))
+  }
+
+  handleBtnClick(e) {
+    // this.setState({
+    //   list: [...this.state.list, this.state.inputValue],
+    //   inputValue: ''
+    // })
+
+    this.setState(prevState => ({
+      list: [...prevState.list, prevState.inputValue],
+      inputValue: ''
+    }))
+  }
+
+  handleItemDelete(index) {
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list: list
+    })
+  }
+}
+
+export default TodoList
---
+++ src/todoItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class TodoItem extends Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  render() {
+    const { content } = this.props
+    return <div onClick={this.handleClick}>{content}</div>
+  }
+
+  handleClick() {
+    const { deleteItem, index } = this.props
+    deleteItem(index)
+  }
+}
+
+TodoItem.propTypes = {
+  content: PropTypes.string.isRequired,
+  index: PropTypes.number,
+  deleteItem: PropTypes.func
+}
+
+TodoItem.defaultProps = {
+  content: '12588'
+}
+export default TodoItem
